22ae2e8680b4f6efdcf84149abf027ddaa9a1df4
Rajangane <pratiksharajangane7@gmail.com>
2021-12-25 12:27:35 +0530

    [Pratiksha] ADD : Deleting the Contacts in Address Book

--- AddressBook_UsingCollection/AddressBook_UsingCollection/ContactDetails.cs
+++ AddressBook_UsingCollection/AddressBook_UsingCollection/ContactDetails.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 
 namespace AddressBook_UsingCollection
 {
-    public class ContactDetails
+    public class ContactDetails//created class for different oprations
     {
+
         //list declaration to store the personal details
         List<Contacts> listcontacts = new List<Contacts>();
         Dictionary<string, Contacts> dic = new Dictionary<string, Contacts>();
         List<Contacts> searchcity = new List<Contacts>();
-       List<Contacts> list1 = new List<Contacts>();//list for city
+        List<Contacts> list1 = new List<Contacts>();//list for city
         List<Contacts> list2 = new List<Contacts>();//list for state
 
 
@@ -48,17 +49,38 @@ namespace AddressBook_UsingCollection
                 phone_no = phone_no,
                 email = email
             });
-
         }
-        /// <summary>
-        /// Edit Method
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="first"></param>
-        /// <param name="last"></param>
-        /// <param name="add"></param>
-        /// <param name="cityN"></param>
-        /// <param name="stateN"></param>
+
+        public void Print()//Print method 
+        {
+            for (int i = 0; i < listcontacts.Count; i++)
+            {
+                Contacts contact = listcontacts[i];
+
+                if (!dic.ContainsKey(contact.first_name))//if condition to check the key is present or not
+                {
+                    dic.Add(contact.first_name, contact);//if not the add into the addressbook
+                }
+                else
+                {
+                    Console.WriteLine(" Name {0} is present", contact.first_name);
+                }
+            }
+            foreach (var i in dic)
+            {
+                Console.WriteLine("First Name : " + i.Key);
+                Console.WriteLine("Last Name : " + i.Value.last_name);
+                Console.WriteLine("Address : " + i.Value.address);
+                Console.WriteLine("City Name : " + i.Value.city);
+                Console.WriteLine("State Name : " + i.Value.state);
+                Console.WriteLine("Zip Code : " + i.Value.zip);
+                Console.WriteLine("Phone No. : " + i.Value.phone_no);
+                Console.WriteLine("Email ID : " + i.Value.email);
+                Console.WriteLine("\n");
+            }
+        }
+
+        //Edit method 
         public void Edit(string name, string first, string last, string add, string cityN, string stateN)
         {
             int indexOfContact = -1;
@@ -79,33 +101,179 @@ namespace AddressBook_UsingCollection
             listcontacts[indexOfContact] = editContact;
 
         }
-        public void Print()//Print method 
+        public void Delete(string firstname) //Delete method
         {
+            int indexOfContact = -1;
             for (int i = 0; i < listcontacts.Count; i++)
             {
-                Contacts contact = listcontacts[i];
-
-                if (!dic.ContainsKey(contact.first_name))//condition to check whether the key is present or not
+                if (listcontacts[i].first_name == firstname)
                 {
-                    dic.Add(contact.first_name, contact);//if not present add into the addressbook
-                }
-                else
-                {
-                    Console.WriteLine(" Name {0} is present", contact.first_name);
+                    indexOfContact = i;
                 }
             }
-            foreach (var i in dic)
+            listcontacts.RemoveAt(indexOfContact);
+
+        }
+        public void SearchContact(string cityName, string statename)
+        {
+            searchcity = listcontacts.FindAll(x => (x.city == cityName || x.state == statename));//Lambda Expression
+
+            foreach (Contacts i in searchcity)
             {
-                Console.WriteLine("First Name : " + i.Key);
-                Console.WriteLine("Last Name : " + i.Value.last_name);
-                Console.WriteLine("Address : " + i.Value.address);
-                Console.WriteLine("City Name : " + i.Value.city);
-                Console.WriteLine("State Name : " + i.Value.state);
-                Console.WriteLine("Zip Code : " + i.Value.zip);
-                Console.WriteLine("Phone No. : " + i.Value.phone_no);
-                Console.WriteLine("Email ID : " + i.Value.email);
-                Console.WriteLine("\n");
+                Console.WriteLine("Person First Name : " + i.first_name);
             }
         }
+        public void SearchCityState(string cityName, string stateName)
+        {
+            list1 = listcontacts.FindAll(x => (x.city == cityName));//to check the names in city
+
+            foreach (Contacts i in list1)
+            {
+                Console.WriteLine("person in {0} City is : {1}", cityName, i.first_name);
+            }
+            list2 = listcontacts.FindAll(x => (x.state == stateName));//to check the Names in State
+            foreach (Contacts i in list2)
+            {
+                Console.WriteLine("person in {0} State is : {1}", stateName, i.first_name);
+            }
+        }
+
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public class ContactDetails
+//    {
+//        //list declaration to store the personal details
+//        List<Contacts> listcontacts = new List<Contacts>();
+//        Dictionary<string, Contacts> dic = new Dictionary<string, Contacts>();
+//        public void Add()//add method 
+//        {
+//            Console.WriteLine("Enter Personal Details : ");
+//            Console.Write("First Name : ");
+//            string first_name = Console.ReadLine();
+//            Console.Write("Last Name : ");
+//            string last_name = Console.ReadLine();
+//            Console.Write("Address : ");
+//            string address = Console.ReadLine();
+//            Console.Write("City : ");
+//            string city = Console.ReadLine();
+//            Console.Write("State : ");
+//            string state = Console.ReadLine();
+//            Console.Write("Zip Code : ");
+//            int zip = Convert.ToInt32(Console.ReadLine());
+//            Console.Write("Phone No. : ");
+//            int phone_no = Convert.ToInt32(Console.ReadLine());
+//            Console.Write("E-mail ID : ");
+//            string email = Console.ReadLine();
+
+//            //adding the details in list
+//            listcontacts.Add(new Contacts()
+//            {
+//                first_name = first_name,
+//                last_name = last_name,
+//                address = address,
+//                city = city,
+//                state = state,
+//                zip = zip,
+//                phone_no = phone_no,
+//                email = email
+//            });
+
+//        }
+//        /// <summary>
+//        /// Edit Method
+//        /// </summary>
+//        /// <param name="name"></param>
+//        /// <param name="first"></param>
+//        /// <param name="last"></param>
+//        /// <param name="add"></param>
+//        /// <param name="cityN"></param>
+//        /// <param name="stateN"></param>
+//        public void Edit(string name, string first, string last, string add, string cityN, string stateN)
+//        {
+//            int indexOfContact = -1;
+//            for (int i = 0; i < listcontacts.Count; i++)
+//            {
+//                if (listcontacts[i].first_name == name)
+//                {
+//                    indexOfContact = i;
+//                }
+//            }
+
+//            var editContact = listcontacts[indexOfContact];
+//            editContact.first_name = first;
+//            editContact.last_name = last;
+//            editContact.address = add;
+//            editContact.city = cityN;
+//            editContact.state = stateN;
+//            listcontacts[indexOfContact] = editContact;
+
+//        }
+//        public void Delete(string firstname) //Delete method
+//        {
+//            int indexOfContact = -1;
+//            for (int i = 0; i < listcontacts.Count; i++)
+//            {
+//                if (listcontacts[i].first_name == firstname)
+//                {
+//                    indexOfContact = i;
+//                }
+//            }
+//            listcontacts.RemoveAt(indexOfContact);
+
+//        }
+//        public void Print()//Print method 
+//        {
+//            for (int i = 0; i < listcontacts.Count; i++)
+//            {
+//                Contacts contact = listcontacts[i];
+
+//                if (!dic.ContainsKey(contact.first_name))//condition to check whether the key is present or not
+//                {
+//                    dic.Add(contact.first_name, contact);//if not present add into the addressbook
+//                }
+//                else
+//                {
+//                    Console.WriteLine(" Name {0} is present", contact.first_name);
+//                }
+//            }
+//            foreach (var i in dic)
+//            {
+//                Console.WriteLine("First Name : " + i.Key);
+//                Console.WriteLine("Last Name : " + i.Value.last_name);
+//                Console.WriteLine("Address : " + i.Value.address);
+//                Console.WriteLine("City Name : " + i.Value.city);
+//                Console.WriteLine("State Name : " + i.Value.state);
+//                Console.WriteLine("Zip Code : " + i.Value.zip);
+//                Console.WriteLine("Phone No. : " + i.Value.phone_no);
+//                Console.WriteLine("Email ID : " + i.Value.email);
+//                Console.WriteLine("\n");
+//            }
+//        }
+//    }
+//}
--- AddressBook_UsingCollection/AddressBook_UsingCollection/Program.cs
+++ AddressBook_UsingCollection/AddressBook_UsingCollection/Program.cs
@@ -1,34 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("************* Address Book Using Collection *****************");
-//Console.WriteLine("************** Address Book Using Collection ***************")
 using AddressBook_UsingCollection;
-Console.Write("Select Number:\n1)AddContacts\n2)EditContacts");
-int option = Convert.ToInt32(Console.ReadLine());
 ContactDetails contactDetails = new ContactDetails();
+int count = 0;//counter to break the while loop
 
-switch (option)
+while (count == 0)
 {
+    Console.Write("Select Number:\n1)AddContacts\n3)EditContacts\n4)DeleteContacts");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
 
-    case 1:
-        contactDetails.Add();
-        contactDetails.Print();
-        break;
-    case 2:
-        Console.WriteLine(" contact which you want to edit :-");
-        Console.WriteLine("Edit Sequence");
-        Console.WriteLine("First name,Last name,Address, City, State");
-        string name = Console.ReadLine();
-        string first = Console.ReadLine();
-        string last = Console.ReadLine();
-        string add = Console.ReadLine();
-        string cityN = Console.ReadLine();
-        string stateN = Console.ReadLine();
+        case 1:
+            contactDetails.Add();
+            
+            break;
+        case 2:
+            contactDetails.Print();
+            break;
 
-        contactDetails.Edit(name, first, last, add, cityN, stateN);//to edit contact details
-        contactDetails.Print();
-        break;
-    default:
-        Console.WriteLine("Invalid Option");
-        break;
+        case 3:
+            Console.WriteLine(" contact which you want to edit :");
+            Console.WriteLine("Edit Sequence");
+            Console.WriteLine("First name,Last name,Address, City, State");
+            string name = Console.ReadLine();
+            string first = Console.ReadLine();
+            string last = Console.ReadLine();
+            string add = Console.ReadLine();
+            string cityN = Console.ReadLine();
+            string stateN = Console.ReadLine();
+
+            contactDetails.Edit(name, first, last, add, cityN, stateN);//to edit contact details
+            contactDetails.Print();
+            break;
+        case 4:
+            Console.WriteLine("Enter First Name To Delete:");
+            string firstname = Console.ReadLine();
+            contactDetails.Delete(firstname);//to delete the contact
+            break;
+        default:
+            Console.WriteLine("Invalid Option");
+            break;
+
+    }
 
 }
